7b95e76eb29cab8a82325cddc7c5e9a17e9b087c
xc145214 <xc145214@gmail.com>
2016-01-13 18:20:26 +0800

    Initial commit

---
+++ content.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2016/1/13.
+ */
+
+module.exports = "it works from content.js! hello world!";
---
+++ entry.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2016/1/13.
+ */
+
+//document.write("it works");
+
+
+//require("!style!css!./style.css");
+
+
+require("./style.css");
+
+document.write(require("./content.js"));
